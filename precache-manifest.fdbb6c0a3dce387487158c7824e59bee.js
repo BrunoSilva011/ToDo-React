bac0dba60a7d31c803c5347f27a49d4de9f7a18f
Bruno Silva <brunoviniciusdasilva11@gmail.com>
2022-09-16 10:33:25 -0300

    Updates

---
+++ precache-manifest.fdbb6c0a3dce387487158c7824e59bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ba377ad79e494332d68",
+    "url": "/ToDo-React/static/css/main.db447007.chunk.css"
+  },
+  {
+    "revision": "4ba377ad79e494332d68",
+    "url": "/ToDo-React/static/js/main.4ba377ad.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/ToDo-React/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "cfca5412d775b3c6f0aa",
+    "url": "/ToDo-React/static/js/2.cfca5412.chunk.js"
+  },
+  {
+    "revision": "54e26afd6ad3266444a4",
+    "url": "/ToDo-React/static/js/runtime~main.54e26afd.js"
+  },
+  {
+    "revision": "7332b85cc85588424323d78211ad5303",
+    "url": "/ToDo-React/index.html"
+  }
+];
